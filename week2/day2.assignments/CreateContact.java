ad241f6994629f7922f8cbcb373de339bec1f7d4
Surenth3 <127432835+Surenth3@users.noreply.github.com>
2023-03-16 17:25:20 +0530

    Update CreateContact.java

--- week2/day2.assignments/CreateContact.java
+++ week2/day2.assignments/CreateContact.java
@@ -4,51 +4,34 @@ public class CreateContact {
 
 	public static void main(String[] args) {
 		
-		/*
-		 * //Pseudo Code
-		 * 
-		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
-		 * 
-		 * 2. Enter UserName and Password Using Id Locator
-		 * 
-		 * 3. Click on Login Button using Class Locator
-		 * 
-		 * 4. Click on CRM/SFA Link
-		 * 
-		 * 5. Click on contacts Button
-		 * 
-		 * 6. Click on Create Contact
-		 *  
-		 * 7. Enter FirstName Field Using id Locator
-		 * 
-		 * 8. Enter LastName Field Using id Locator
-		 * 
-		 * 9. Enter FirstName(Local) Field Using id Locator
-		 * 
-		 * 10. Enter LastName(Local) Field Using id Locator
-		 * 
-		 * 11. Enter Department Field Using any Locator of Your Choice
-		 * 
-		 * 12. Enter Description Field Using any Locator of your choice 
-		 * 
-		 * 13. Enter your email in the E-mail address Field using the locator of your choice
-		 * 
-		 * 14. Select State/Province as NewYork Using Visible Text
-		 * 
-		 * 15. Click on Create Contact
-		 * 
-		 * 16. Click on edit button 
-		 * 
-		 * 17. Clear the Description Field using .clear
-		 * 
-		 * 18. Fill ImportantNote Field with Any text
-		 * 
-		 * 19. Click on update button using Xpath locator
-		 * 
-		 * 20. Get the Title of Resulting Page.
-
-         */
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+		ChromeDriver driver=new ChromeDriver();
 		
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("DemoCSR");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'CRM')]")).click();
+		driver.findElement(By.xpath("//*[text() ='Leads']")).click();
+		driver.findElement(By.xpath("//*[text() ='Create Lead']")).click();
+		driver.findElement(By.xpath("//input[@id='createLeadForm_companyName']")).sendKeys("Infosys");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).sendKeys("Surenth");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_lastName']")).sendKeys("R");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstNameLocal']")).sendKeys("Suren");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_departmentName']")).sendKeys("Testing");
+		driver.findElement(By.xpath("//textarea[@id='createLeadForm_description']")).sendKeys("Entering the automation testing");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_primaryEmail']")).sendKeys("prsurenth@gmail.com");
+		Select select = new Select (driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId")));
+		select.selectByVisibleText("New York");
+		driver.findElement(By.xpath("//input[@name='submitButton']")).click();
+		driver.findElement(By.xpath("//*[text() ='Edit']")).click();
+		driver.findElement(By.xpath("//textarea[@id='updateLeadForm_description']")).clear();
+		driver.findElement(By.xpath("//textarea[@id='updateLeadForm_importantNote']")).sendKeys("Entering the automation testing");
+		driver.findElement(By.xpath("//input[@name='submitButton']")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
 	}
 
 }
